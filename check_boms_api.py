286d5eea89908fb439df0a72eb71463937df2876
YusufPEKERR <88205850+YusufPEKERR@users.noreply.github.com>
2026-07-31 07:55:29 +0300

    chore: Kullanılmayan dosya ve klasörler projeden temizlendi

--- check_boms_api.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-sys.path.append(os.getcwd())
-from core.web_bridge import WebBridge
-bridge = WebBridge()
-print(bridge.get_item_boms()[:200]) # just print the first 200 chars to avoid huge output
